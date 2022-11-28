87e6646e33b8f834f93ed1e6e76dc67c2aff3899
Sevastianova Irina <Sevastianova21@gmail.com>
2022-11-28 23:31:37 +0300

    Первая задача

---
+++ homework01/Ex1/Program.cs
@@ -0,0 +1,12 @@
+﻿
+Console.WriteLine("Задайте 2 числа");
+Console.WriteLine("Введите первое число");
+string FirstNumberStr = Console.ReadLine();
+Console.WriteLine("Введите второе число");
+string SecondNumberStr = Console.ReadLine();
+int FirstNumber = Convert.ToInt32(FirstNumberStr);
+int SecondNumber = Convert.ToInt32(SecondNumberStr);
+if (FirstNumber > SecondNumber)
+    {Console.WriteLine($"max = {FirstNumber}");}
+else 
+    {Console.WriteLine($"max = {SecondNumber}");}
