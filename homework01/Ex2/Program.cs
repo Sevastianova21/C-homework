f1276ce4ecf6af3e6f34e658215e344559f7c47d
Sevastianova Irina <Sevastianova21@gmail.com>
2022-11-30 21:21:34 +0300

    Вторая задача

---
+++ homework01/Ex2/Program.cs
@@ -0,0 +1,16 @@
+﻿Console.WriteLine("Задайте 3 числа");
+Console.WriteLine("Введите первое число");
+string FirstNumberStr = Console.ReadLine();
+Console.WriteLine("Введите второе число");
+string SecondNumberStr = Console.ReadLine();
+Console.WriteLine("Введите третье число");
+string ThirdNumberStr = Console.ReadLine();
+int FirstNumber = Convert.ToInt32(FirstNumberStr);
+int SecondNumber = Convert.ToInt32(SecondNumberStr);
+int ThirdNumber = Convert.ToInt32(ThirdNumberStr);
+int Maxima = FirstNumber;
+if (SecondNumber > Maxima)
+    {Maxima = SecondNumber;}
+if (ThirdNumber > Maxima)
+    {Maxima = ThirdNumber;}
+Console.WriteLine($"max = {Maxima}");
