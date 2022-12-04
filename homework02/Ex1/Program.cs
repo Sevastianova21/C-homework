3331cd8244a0e4e164bf44001a775ca33d9024b0
Sevastianova Irina <Sevastianova21@gmail.com>
2022-12-04 19:44:48 +0300

    Домашка ко второму семинару

---
+++ homework02/Ex1/Program.cs
@@ -0,0 +1,6 @@
+﻿// Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+Console.WriteLine("Введите трехзначное число");
+string NumberStr = Console.ReadLine();
+int Number = Convert.ToInt32(NumberStr);
+int Digit = Number/10%10;
+Console.WriteLine($"Вторая цифра {Digit}");
---
+++ homework02/Ex2/Program.cs
@@ -0,0 +1,9 @@
+﻿// Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+Console.WriteLine("Введите число");
+string NumberStr = Console.ReadLine();
+int Number = Convert.ToInt32(NumberStr); // избавляемся от нулей спереди, если кто то решил ввести  
+string QNumberStr = Convert.ToString(Number);
+if (Number < 100){ 
+    Console.WriteLine("Третей цифры нет");}
+else {
+    Console.WriteLine($"Третья цифра {QNumberStr[2]}");}
